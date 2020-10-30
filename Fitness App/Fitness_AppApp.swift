df57ebeb2684a643d08c7173ba2fd71b539e3de3
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-10-30 21:02:34 +0330

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Mohsen Abdollahi on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
